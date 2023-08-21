26302dae88d4a45ebf179148b2c6bd400a2f7250
sol0916 <yj0916lee00@gmail.com>
2023-08-21 20:48:17 +0900

    20230821 commit

--- src/main/java/com/coding404/myweb/controller/UserController.java
+++ src/main/java/com/coding404/myweb/controller/UserController.java
@@ -1,12 +1,22 @@
 package com.coding404.myweb.controller;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.coding404.myweb.util.KaKaoAPI;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+	
+	@Autowired
+	private KaKaoAPI kakaoAPI;
+	
 
 	@GetMapping("/join")
 	public String join() {
@@ -21,6 +31,24 @@ public class UserController {
 	@GetMapping("/userDetail")
 	public String detail() {
 		return "user/userDetail";
+	}
+	
+	//카카오로그인
+	@GetMapping("/kakao")
+	public String kakao(@RequestParam("code") String code) {
+		
+		//토큰 받기
+		String token = kakaoAPI.getToken(code);
+		
+		//유저 정보 받기
+		Map<String, Object> map = kakaoAPI.getUser(token);
+		
+		System.out.println("카카오에서 받은 사용자 정보:" + map.toString());
+		
+		//우리 데이터베이스에서 조회 or 확인 -> 가입으로 연결 or 로그인처리
+		
+		
+		return "redirect:/main"; //메인화면으로
 	}
 	
 }
